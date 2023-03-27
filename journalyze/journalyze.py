b39a5611b4942082df82c2b01f0845cba0690db7
Johana De La Rosa <68411727+jedlr@users.noreply.github.com>
2023-03-26 23:24:38 -0400

    Update journalyze.py

--- journalyze/journalyze.py
+++ journalyze/journalyze.py
@@ -1,6 +1,7 @@
 import csv
 import random
 
+
 class DailyPrompt:
     # initialize the object and load the prompts from a CSV file.
     def __init__(self, prompts_file):
@@ -11,14 +12,17 @@ class DailyPrompt:
             for row in reader:
                 self.prompts.append(row[0])
 
+                
     # randomly select a prompt from the list of prompts and return it to the user.
     def get_prompt(self):
         return random.choice(self.prompts)
 
+    
     # allow the user to add a new prompt to the list of prompts.
     def add_prompt(self, prompt):
         self.prompts.append(prompt)
 
+        
     # allow the user to remove a prompt from the list of prompts.
     def remove_prompt(self, prompt):
         self.prompts.remove(prompt)
