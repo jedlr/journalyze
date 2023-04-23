830776cf4f92ef8258780c8923128cf7511f4dba
Johana De La Rosa <68411727+jedlr@users.noreply.github.com>
2023-04-23 02:10:39 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = "sphinx_rtd_theme"
-html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 def setup(app):
